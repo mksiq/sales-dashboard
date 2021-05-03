fd489ba8c99b41893949450666a5edbe1465ba58
mksiq <maickelsiqueira@gmail.com>
2021-05-03 11:23:10 -0400

    added footer component to fronend and icons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
-import NavBar from 'components/NavBar'
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div className="container">
-        <h1>Sales Dashboard</h1>
+    <>
+      <div className="App">
+        <NavBar />
+        <div className="container">
+          <h1>Sales Dashboard</h1>
+        </div>
+      
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light"><small>App developed by <a href="https://www.maickel.ca/" className="link-light" target="_blank" rel="noreferrer">Maickel Siqueira</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

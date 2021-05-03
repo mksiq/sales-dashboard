9cbd7086e1d3491f2ffef14a3a889cb2d772464f
mksiq <maickelsiqueira@gmail.com>
2021-05-03 11:06:47 -0400

    added logo and navbar component on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NavBar from 'components/NavBar'
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-primary">Sales Dashboard</h1>
+      <NavBar />
+      <div className="container">
+        <h1>Sales Dashboard</h1>
+      </div>
     </div>
   );
 }

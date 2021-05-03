4c62b3438533acefc20fe9f73253996354b2884a
mksiq <maickelsiqueira@gmail.com>
2021-05-03 08:37:34 -0400

    Project initialized with Spring for backend

---
+++ backend/src/main/java/ca/maickel/salesds/SalesdsApplication.java
@@ -0,0 +1,13 @@
+package ca.maickel.salesds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesdsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ca/maickel/salesds/SalesdsApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.maickel.salesds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

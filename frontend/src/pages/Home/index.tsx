515c5414177d230e2045c7b659593408c001a1df
mksiq <maickelsiqueira@gmail.com>
2021-05-07 14:37:06 -0400

    frontend: added navigation to home and dashboard

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,26 @@
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+import { Link } from 'react-router-dom';
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-     
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">SalesDS</h1>
+          <p className="lead">Check the performance of your sales.</p>
+          <hr />
+          <p>
+            This app displays a dashboard with data supplied by a Spring Boot
+            server.
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+            Access Dashboard
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 

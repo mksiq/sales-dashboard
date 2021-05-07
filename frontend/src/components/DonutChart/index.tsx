f4fbfbd7babfa2707ed3b9a606c705c394a57f5c
mksiq <maickelsiqueira@gmail.com>
2021-05-07 15:23:51 -0400

    frontend: donut chart fetch data from backend

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,22 +1,47 @@
 import Chart from 'react-apexcharts';
+import axios from 'axios';
+import { BASE_URL } from 'utils/requests';
+import { SaleSum } from 'types/sale';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-function BarChart() {
-  const mockData = {
-    series: [80841, 45031, 42047, 95010, 35400],
-    labels:  ['Nero', 'Julius Caesar', 'Gaius', 'Augustus', 'Constantine'],
-  }
+type ChartData = {
+  labels: Array<string>;
+  series: Array<number>;
+};
+
+function DonutChart() {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: [],
+    series: [],
+  });
+
+  useEffect(() => {
+    try {
+      (async () => {
+        const res = await axios.get(`${BASE_URL}/sales/total-by-salesman`);
+        const data = res.data as Array<SaleSum>;
+        setChartData({
+          labels: data.map((saleSum) => saleSum.salesman),
+          series: data.map((saleSum) => saleSum.value),
+        });
+      })();
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
 
   const options = {
     legend: {
-      show: true
-    }
+      show: true,
+    },
   };
 
   return (
     <div>
       <Chart
-        options={{ ...options, labels: mockData.labels }}
-        series={mockData.series}
+        options={{ ...options, labels: chartData.labels }}
+        series={chartData.series}
         type="donut"
         height="250"
       />
@@ -24,4 +49,4 @@ function BarChart() {
   );
 }
 
-export default BarChart;
+export default DonutChart;
---
+++ frontend/src/types/sale.ts
@@ -0,0 +1,4 @@
+export type SaleSum = {
+  salesman: string;
+  value: number;
+};
